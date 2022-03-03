599d9ffba8f6b6c1430c8cf872a64d3a378b6402
Lee Jun <leejun.nakao@gmail.com>
2022-03-03 09:41:19 -0300

    feat: create main.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,10 @@
+export {
+  HookSetState,
+  InputProps,
+  SelectOption,
+  SelectProps,
+} from "./components/protocols";
+
+export { Config } from "./form-builder";
+
+export { InputsOptions } from "./form-builder/fn";
